7404168bf4932690b4592478a7d925ee79cb6448
Tanmay Khandelwal <iamtanmaykh07@gmail.com>
2023-04-22 17:44:26 +0530

    testimionaial section contact us section and footer added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Home from "./components/home/Home";
 import Skills from "./components/skills/Skills";
 import Service from "./components/services/Services";
 import Qualification from "./components/qualification/Qualification";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import ScrollUp from "./components/scrollup/ScrollUp";
 
 function App() {
     return (
@@ -16,7 +20,11 @@ function App() {
                 <Skills/>
                 <Service/>
                 <Qualification/>
+                <Testimonials/>
+                <Contact/>
             </main>
+            <Footer/>
+            <ScrollUp/>
         </>
     );
 }
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import './footer.css'
 
-const footer = () => {
-    return (
-        <div>footer</div>
-    )
+const Footer = () => {
+  return (
+    <footer className='footer'>
+        <div className="footer__container container">
+            <h2 className='footer__title'>Tanmay</h2>
+
+            <ul className='footer__list'>
+                <li>
+                    <a href="#about" className='footer__link'>About</a>
+                </li>
+                <li>
+                    <a href="#portfolio" className='footer__link'>Projects</a>
+                </li>
+                <li>
+                    <a href="#about" className='footer__link'>Testimonials</a>
+                </li>
+            </ul>
+
+            <div className='footer__social'>
+                <a href="https://github.com/itstanmaykhandelwal" className='footer__social-icon' target='_blank' rel="noreferrer">
+                    <i class="bx bxl-github"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/itstanmaykhandelwal/" className='footer__social-icon' target='_blank' rel="noreferrer">
+                    <i class="bx bxl-linkedin"></i>
+                </a>
+                <a href="https://www.instagram.com/itstanmaykhandelwal/" className='footer__social-icon' target='_blank' rel="noreferrer">
+                    <i class="bx bxl-instagram"></i>
+                </a>   
+            </div>
+            <span className='footer__copy'>
+                &#169; Tanmay Khandelwal. All rights reserved
+            </span>
+        </div>
+    </footer>
+  )
 }
 
-export default footer
+export default Footer
